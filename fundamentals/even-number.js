f68b40e75218e6004a870d1879c7acb36bfdc424
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2023-08-04 17:40:51 -0300

    even number function
    A function that takes a as parameter and return true is a is even, or false otherwise.

---
+++ fundamentals/even-number.js
@@ -0,0 +1,8 @@
+function myFunction(a) {
+    return a%2===0;
+}
+
+console.log(myFunction(10)); // returns true 
+console.log(myFunction(-4)); // returns true
+console.log(myFunction(5)); // returns false
+console.log(myFunction(-111)); // returns false
