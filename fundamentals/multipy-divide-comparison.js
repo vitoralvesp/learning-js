010005b11678a4852db14014fe08dffbd4bb98c9
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2023-08-04 19:59:04 -0300

    comparison between 2 numbers
    A function that receives a and b as parameters and if b is greater than the function will return the division of a by b. Otherwise, it will return the multiplication of a by b.

---
+++ fundamentals/multipy-divide-comparison.js
@@ -0,0 +1,8 @@
+function myFunction(a,b) {
+    return a<b ? a/b : a*b
+}
+
+console.log(myFunction(10,100)); // return 0.1
+console.log(myFunction(90,45)); // return 4050
+console.log(myFunction(8,20)); // return 0.4
+console.log(myFunction(2,.5)); // return 1
