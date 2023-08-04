64ac95f6f537e97b9602618e77528568370d6bd8
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2023-08-04 15:47:14 -0300

    ex11
    A function that takes two values a and b and returns the percentage of b in a

---
+++ fundamentals/percentage-of-number.js
@@ -0,0 +1,7 @@
+function myFunction(a,b) {
+    return b*(a/100)
+}
+
+console.log(myFunction(100,50)); // returns 50
+console.log(myFunction(10,1)); // returns 0.1
+console.log(myFunction(500,25)); // returns 125
