5d11e2053c6eb243a261879374230ceaa472e631
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2023-08-04 16:15:27 -0300

    math operator
    A function that takes a,b,c,d,e and f as parameters and returns the sum of a and b subtracted by c then multiplied by d and divided by e raised by f

---
+++ fundamentals/math-operators.js
@@ -0,0 +1,7 @@
+function myFunction(a,b,c,d,e,f) {
+    return ((((a+b)-c)*d)/e)**f
+}
+
+console.log(myFunction(6,5,4,3,2,1)) // returns 10.5
+console.log(myFunction(6,2,1,4,2,3)) // returns 2744
+console.log(myFunction(2,3,6,4,2,3)) // returns -8
